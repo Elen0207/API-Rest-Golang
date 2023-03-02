5904d4f681840026e731a948b117060a42dc3b23
Elen <elen.oliveira@finanzero.com.br>
2023-03-01 21:11:38 -0300

    finished api

--- main.go
+++ main.go
@@ -3,15 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/Elen0207/API-Rest-Golang/models"
+	"github.com/Elen0207/API-Rest-Golang/database"
 	"github.com/Elen0207/API-Rest-Golang/routes"
 )
 
 func main() {
-	models.Personalidades = []models.Personalidade{
-		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
-		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
-	}
+	database.ConectaComBancoDeDados()
 	fmt.Println("Initializing server...")
 	routes.HandleResquest()
 }
--- routes/routes.go
+++ routes/routes.go
@@ -4,14 +4,19 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
 	"github.com/Elen0207/API-Rest-Golang/controllers"
+	"github.com/Elen0207/API-Rest-Golang/middleware"
+	"github.com/gorilla/mux"
 )
 
 func HandleResquest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
+	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("Post")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
